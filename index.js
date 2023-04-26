c696a752333f6f420a9d476599ef5b24b0ec1426
josh4got <125082025+josh4got@users.noreply.github.com>
2023-04-26 11:26:36 -0600

    init files, tests/classes for shapes

---
+++ index.js
@@ -0,0 +1,12 @@
+// require needed files (inquirer, fs)
+
+// prompt user for input - logo text up to 3 characters
+// prompt user for input - text color keyword / hexa code
+
+// prompt user for input - Shape (list options: circle, square, triangle)
+// prompt user for input - Shape Color keyword / hexa code
+
+// create SVG file named logo.svg
+// console log "Generated logo.svg"
+
+// logo should be 300x200 pixels matching the user's input criteria
